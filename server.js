caf358701cf762b5c0a8be19b24f4b0bb2517348
Nam Ninja <namninja@gmail.com>
2019-02-10 20:04:26 -0800

    Added POST endpoint for Recipes

--- server.js
+++ server.js
@@ -47,11 +47,26 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
-
+app.post('/recipes', jsonParser, (req, res) => {
+  //ensure name and ingredients are in request body
+  const requiredFields = ['name', 'ingredients'];
+  //search through required fields
+  for (let i =0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    //if any fields are missing, send back and error message
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  //if both required fields are present, then create the recipe
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+})
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
